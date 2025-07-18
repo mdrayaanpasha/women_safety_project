6faa0c9cf72f1f36c779014bc63b2589dfbbaaf2
mdrayaanpasha <134199206+mdrayaanpasha@users.noreply.github.com>
2025-07-18 18:14:21 +0530

    added client

---
+++ CLIENT/app/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>HEllow orld</h1>} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
